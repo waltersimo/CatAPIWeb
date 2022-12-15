34b40e26b24a67cb897c8939df8da97c13d7268e
walter_ <simowalter01@gmail.com>
2022-12-15 16:55:48 +0100

    version 1.1 (Latest)

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,17 +5,29 @@ import com.squareup.otto.ThreadEnforcer;
 import org.example.Controller.CatController;
 import org.example.IHM.CatIHM;
 
+import javax.imageio.ImageIO;
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.net.URL;
 import javax.swing.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 
 public class Main {
 
     public static final String URL_RANDOM_CAT = "https://api.thecatapi.com/v1/images/search?limit=10&breed_ids=beng&api_key=live_LThZH9F7j7Jf9GqaE5fjcFxgD24XFHQsoYZux8exFIgvAkSoMAaNa7M4htF4gc79";
-
+//    public static final String URL_RANDOM_CAT = "https://api.thecatapi.com/v1/images/search?limit=10";
     public static final Bus bus = new Bus(ThreadEnforcer.ANY);
 
+    public static void main(String[] args) throws IOException, URISyntaxException {
 
-    public static void main(String[] args) {
         JFrame frame = new JFrame ("MyPanel");
+        frame.setTitle("API Cat App");
+        frame.setResizable(true);
+        frame.setSize(1200, 700);
+        frame.revalidate();
+
         frame.setDefaultCloseOperation (JFrame.EXIT_ON_CLOSE);
         CatIHM Catihm = new CatIHM();
 
